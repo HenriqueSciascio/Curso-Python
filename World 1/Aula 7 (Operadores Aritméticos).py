969c21297fdc9cbf453f2a99cd2b9b0552341351
Henrique Sciascio <henrique.sciascio@gmail.com>
2023-08-01 23:26:03 -0300

    Update aula 7

--- World 1/Aula 7 (Operadores Aritméticos).py
+++ World 1/Aula 7 (Operadores Aritméticos).py
@@ -1,2 +1,74 @@
 #exercicio 5
+"""
 x = int(input('escolha um numero: '))
+
+print(f'Analisando o valor {x}, seu antecessor é {x-1} e o sucessor é {x+1}')
+"""
+#exercicio 6
+"""
+n = int(input('digite um numero: '))
+
+print(f'o dobro de {n} é {n*2}\n'
+      f'o triplo de {n} é {n*3}\n'
+      f'a raiz quadrada de {n} é {n**0.5:.2f}')
+"""
+#exercicio 7
+"""
+n1 = float(input('digite a primeira nota do aluno: '))
+n2 = float(input('digite a segunda nota do aluno: '))
+
+print(f'a média entre {n1} e {n2} é igual a {(n1+n2)/2:.1f}')
+"""
+#exercicio 8
+"""
+n = float(input('uma distancia em metros: '))
+
+print(f'a medida de {n}m corresponde a\n'
+      f'{n/1000}Km\n'
+      f'{n/100}Hm\n'
+      f'{n/10}Dam\n'
+      f'{n*10:.0f}Dm\n'
+      f'{n*100:.0f}Cm\n'
+      f'{n*1000:.0f}Mm\n')
+"""
+#exercicio 9
+"""x = int(input('Digite um numero para ver sua tabuada: '))
+
+print('-'*20)
+print(f'{x} x 1 = {x}\n'
+      f'{x} x 2 = {x*2}\n'
+      f'{x} x 3 = {x*3}\n'
+      f'{x} x 4 = {x*4}\n'
+      f'{x} x 5 = {x*5}\n'
+      f'{x} x 6 = {x*6}\n'
+      f'{x} x 7 = {x*7}\n'
+      f'{x} x 8 = {x*8}\n'
+      f'{x} x 9 = {x*9}\n'
+      f'{x} x 10 = {x*10}')
+print('-'*20)
+"""
+#exercicio 10
+"""
+x = float(input('quanto dinheiro você tem na carteira? R$'))
+
+print(f'com R${x} você pode comprar US${x/3.27:.2f}')
+"""
+#exercicio 11
+"""
+lar = float(input('Qual que é a Largura da parede em metros? '))
+alt = float(input('Qual que é a Altura da parede em metros? '))
+
+print(f'A área da parede é {lar*alt}\ne a quantidade de tinta necessaria para pinta-la será de {(lar*alt/2)}L')
+"""
+#exercicio 12
+"""
+x = float(input('escreva o preço do produto: '))
+
+print(f'um produto que custa {x}, com um desconto de 5%, vai custar {x-(x*0.05)}')
+"""
+#exercicio 13
+"""
+x = float(input('escreva seu salario: '))
+
+print(f'seu salario atual: {x}\nseu salario com 15% de aumento:{x+(x*0.15)}')
+"""
