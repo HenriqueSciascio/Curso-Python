79b72dea8c0b0963ee0b68b6a096d701e0e54b54
Henrique Sciascio <henrique.sciascio@gmail.com>
2023-08-06 19:01:52 -0300

    Update projetos pessoais

--- Mandar msg automatica com Pyautogui.py
+++ Mandar msg automatica com Pyautogui.py
@@ -15,8 +15,9 @@ time.sleep(2)
 pg.moveTo(-1260, 1227)
 pg.leftClick()
 time.sleep(2)
-for c in range(0, 77):
-    pg.write('gay')
+for c in range(0, 1):
+    pg.write('ola amigo')
     time.sleep(0.1)
     pg.press('enter')
     time.sleep(0.1)
+pg.alert(text='trabalho concluido', title='sucesso', button='ok')
