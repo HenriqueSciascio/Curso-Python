08298d6ab3f421f3a53b2dc65d880cfca07cdf64
Henrique Sciascio <henrique.sciascio@gmail.com>
2023-08-13 19:23:07 -0300

    Aula 11 (cores)

---
+++ World 1/Aula 11 (cores).py
@@ -0,0 +1,15 @@
+print('\033[0;33;44mOla, Mundo!')
+
+"""
+estilo          texto               backgrond
+0 = none        30 = branco         40 = branco
+1 = bold        31 = vermelho       41 = vermelho
+4 = underline   32 = verde          42 = verde
+7 = negative    33 = amarelo        43 = amarelo
+                34 = azul escuro    44 = azul escuro
+                35 = lilas          45 = lilas
+                36 = azul claro     46 = azul claro
+                37 = cinza          47 = cinza
+"""
+print('\033[m')
+print('\033[7;30mletra preta')
